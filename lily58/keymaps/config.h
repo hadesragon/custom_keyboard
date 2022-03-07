07afa5da65cf99f6c1582df4e6e229de6a8fad82
jt.j <jt.j@kakaoenterprise.com>
2022-03-07 15:07:24 +0900

    lily58 config fix

--- lily58/keymaps/config.h
+++ lily58/keymaps/config.h
@@ -27,8 +27,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 
+#ifdef RGBLIGHT_ENABLE
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 27
@@ -36,6 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
+#endif
 
 // Underglow
 /*
